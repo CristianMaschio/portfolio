16a0aafec83189f792cdb69a20d8c8c330401bbc
CristianMaschio <blackspartan@hotmail.it>
2019-01-30 22:56:10 +0100

    [Paragraph] added improvements to the code

--- src/pages/_shared/Paragraph/index.js
+++ src/pages/_shared/Paragraph/index.js
@@ -2,43 +2,63 @@ import React, { PureComponent } from "react"
 import "./Paragraph.css"
 
 export default class Paragraph extends PureComponent {
+  state = {
+    isLeft: undefined
+  }
+
+  componentDidMount(){
+    this.setState({isLeft: this.props.index % 2 === 0})
+  }
+
+  //----------Functions-----------
+
+  renderParagraphContent = () => {
+    const { title, image, sections } = this.props
+    const isLeft = this.state.isLeft
+
+    return(
+      <div className="paragraph">
+        <h2
+          className={
+            isLeft ? "paragraphTitle leftTitle" : "paragraphTitle rightTitle"
+          }
+        >
+          {title}
+        </h2>
+        <div
+          className={
+            isLeft
+              ? "paragraphContainer leftParagraph"
+              : "paragraphContainer rightParagraph"
+          }
+        >
+          <div className="sectionsContainer">
+            {sections.map((section, index) => {
+              return (
+                <div key={index}>
+                  <p>{section.date}</p>
+                </div>
+              )
+            })}
+          </div>
+          <img
+            className="paragraphImage"
+            alt="Universtity of Padua logo"
+            src={image && require("../../../../public/images/" + image)}
+          />
+        </div>
+      </div>
+    )
+  }
+
+  //------------Render--------------
+
   render() {
-    const { title, image, sections, index } = this.props
-    const isLeft = index % 2 === 0
+    const isLeft = this.state.isLeft
     return (
       <>
         <div className={isLeft ? "leftLine" : "rightLine"} />
-        <div className="paragraph">
-          <h2
-            className={
-              isLeft ? "paragraphTitle leftTitle" : "paragraphTitle rightTitle"
-            }
-          >
-            {title}
-          </h2>
-          <div
-            className={
-              isLeft
-                ? "paragraphContainer leftParagraph"
-                : "paragraphContainer rightParagraph"
-            }
-          >
-            <div className="sectionsContainer">
-              {sections.map((section, index) => {
-                return (
-                  <div key={index}>
-                    <p>{section.date}</p>
-                  </div>
-                )
-              })}
-            </div>
-            <img
-              className="paragraphImage"
-              alt="Universtity of Padua logo"
-              src={image && require("../../../../public/images/" + image)}
-            />
-          </div>
-        </div>
+        {this.renderParagraphContent()}
       </>
     )
   }
