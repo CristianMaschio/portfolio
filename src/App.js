823368c5bd4f30e55b5091a6ec8e1dca8ef352ad
CristianMaschio <blackspartan@hotmail.it>
2019-02-06 22:51:00 +0100

    [Footer] start to add footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import "./styles/global.scss"
 import Home from "./pages/Home"
 import Header from "./_shared/Header"
+import Footer from "./_shared/Footer"
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <>
         <Header />
         <Home />
+        <Footer />
       </>
     )
   }
