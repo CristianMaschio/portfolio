0e102d51ec1d52e1f9b37dd08c09925140e44045
CristianMaschio <blackspartan@hotmail.it>
2019-04-14 17:52:40 +0200

    [Header] change class to context

--- src/pages/_shared/Header/index.js
+++ src/pages/_shared/Header/index.js
@@ -1,5 +1,5 @@
 import "./Header.scss"
-import React, { PureComponent } from "react"
+import React, { useContext } from "react"
 import Icon from "antd/lib/icon"
 
 import ImageProfile from "../ImageProfile"
@@ -13,92 +13,83 @@ const personalData = [
   { key: "Nationality", text: "Italy" }
 ]
 
-export class Header extends PureComponent {
-  renderContent = contents => {
-    return (
-      <div>
-        {contents.map(content => {
-          return (
-            <div key={content.key} className="menuContent">
-              <div className="line" />
-              <div className="menuContentContainer">
-                <p>{content.key}:</p>
-                <p>{content.text}</p>
-              </div>
-            </div>
-          )
-        })}
-      </div>
-    )
-  }
-
-  render() {
-    const { setRenderInformation, headerClass } = this.props.context
-    return (
-      <header className={headerClass}>
-        <div onClick={() => setRenderInformation(false)} className="close" />
-        <div className="header-blue" />
-        <div className="header-information">
-          <ImageProfile size={200} />
-          <div className="menuContainer">
+function HeaderContent() {
+  return (
+    <div>
+      {personalData.map(content => {
+        return (
+          <div key={content.key} className="menuContent">
             <div className="line" />
-            <h2>Personal</h2>
-            {this.renderContent(personalData)}
-          </div>
-          <div className="menuContainer">
-            <div className="line" />
-            <h2>Contacts</h2>
-            <div className="menuContent">
-              <div className="line" />
-              <div className="menuContentContainer">
-                <a
-                  href="tel:345-273-6368"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <Icon type="phone" />
-                </a>
-                <a
-                  href="mailto:cristianmaschio@hotmail.it"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <Icon type="mail" />
-                </a>
-                <a
-                  href="https://github.com/CristianMaschio"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <Icon type="github" />
-                </a>
-                <a
-                  href="https://www.linkedin.com/in/cristian-maschio-37004a146"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <Icon type="linkedin" />
-                </a>
-              </div>
+            <div className="menuContentContainer">
+              <p>{content.key}:</p>
+              <p>{content.text}</p>
             </div>
           </div>
-          <div className="eu-regolation">
-            I authorize the processing of my personal data pursuant to
-            Legislative Decree 196/03 and the General Data Protection Regulation
-            (EU / 2016/679).
-          </div>
-        </div>
-      </header>
-    )
-  }
+        )
+      })}
+    </div>
+  )
 }
 
-export default class HeaderConsumer extends PureComponent {
-  render() {
-    return (
-      <AppState.Consumer>
-        {state => <Header context={state} />}
-      </AppState.Consumer>
-    )
-  }
+function HeaderContacts() {
+  return (
+    <div className="menuContent">
+      <div className="line" />
+      <div className="menuContentContainer">
+        <a href="tel:345-273-6368" target="_blank" rel="noopener noreferrer">
+          <Icon type="phone" />
+        </a>
+        <a
+          href="mailto:cristianmaschio@hotmail.it"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Icon type="mail" />
+        </a>
+        <a
+          href="https://github.com/CristianMaschio"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Icon type="github" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/cristian-maschio-37004a146"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Icon type="linkedin" />
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default function Header() {
+  const { headerClass, setRenderInformation } = useContext(AppState)
+
+  return (
+    <header className={headerClass}>
+      <div onClick={() => setRenderInformation(false)} className="close" />
+      <div className="header-blue" />
+      <div className="header-information">
+        <ImageProfile size={200} />
+        <div className="menuContainer">
+          <div className="line" />
+          <h2>Personal</h2>
+          <HeaderContent />
+        </div>
+        <div className="menuContainer">
+          <div className="line" />
+          <h2>Contacts</h2>
+          <HeaderContacts />
+        </div>
+        <div className="eu-regolation">
+          I authorize the processing of my personal data pursuant to Legislative
+          Decree 196/03 and the General Data Protection Regulation (EU /
+          2016/679).
+        </div>
+      </div>
+    </header>
+  )
 }
