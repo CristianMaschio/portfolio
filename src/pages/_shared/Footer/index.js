ffa03b86a19def63d2f25e60ed9dc06546fda9a0
CristianMaschio <blackspartan@hotmail.it>
2019-04-15 21:44:11 +0200

    [Footer] change footer class to hooks

--- src/pages/_shared/Footer/index.js
+++ src/pages/_shared/Footer/index.js
@@ -1,54 +1,50 @@
-import React, { PureComponent } from "react"
+import React, { useContext } from "react"
 import "./Footer.scss"
 
-export default class Footer extends PureComponent {
-  render() {
-    return (
-      <footer>
-        <ul>
-          <li>
-            <a
-              href="tel:345-273-6368"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              +39 3452736368
-            </a>
-          </li>
-          <li>
-            <a
-              href="mailto:cristianmaschio@hotmail.it"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              CristianMaschio@hotmail.it
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.linkedin.com/in/cristian-maschio-37004a146"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              https://www.linkedin.com/in/cristian-maschio-37004a146
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://github.com/CristianMaschio"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              https://github.com/CristianMaschio
-            </a>
-          </li>
-          <li>
-            I authorize the processing of my personal data pursuant to
-            Legislative Decree 196/03 and the General Data Protection Regulation
-            (EU / 2016/679).
-          </li>
-        </ul>
-      </footer>
-    )
-  }
+import AppState from "../../../gState/context"
+
+export default function Footer() {
+  const { language } = useContext(AppState)
+  const privacyTerms = require(`../../../data/${language}-content.json`)
+    .privacyTerms
+
+  return (
+    <footer>
+      <ul>
+        <li>
+          <a href="tel:345-273-6368" target="_blank" rel="noopener noreferrer">
+            +39 3452736368
+          </a>
+        </li>
+        <li>
+          <a
+            href="mailto:cristianmaschio@hotmail.it"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            CristianMaschio@hotmail.it
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/cristian-maschio-37004a146"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            https://www.linkedin.com/in/cristian-maschio-37004a146
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/CristianMaschio"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            https://github.com/CristianMaschio
+          </a>
+        </li>
+        <li>{privacyTerms}</li>
+      </ul>
+    </footer>
+  )
 }
