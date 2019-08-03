b162cc5c5f1c48f57eec105f9f24ff85d5450239
CristianMaschio <blackspartan@hotmail.it>
2019-08-03 13:24:46 +0200

    Updates

--- precache-manifest.95453f0ca768739c1ee975d6c46505e2.js
+++ precache-manifest.95453f0ca768739c1ee975d6c46505e2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = [
   {
-    "revision": "ee7b96fa85d8fdb8c126409326ac2d2b",
-    "url": "/portfolio/static/media/Roboto-Bold.ee7b96fa.ttf"
+    "revision": "89e2666c24d37055bcb60e9d2d9f7e35",
+    "url": "/portfolio/static/media/Roboto-Thin.89e2666c.ttf"
   },
   {
-    "revision": "1b75715b7eb71dc749de",
+    "revision": "2d3dd869de5cc9890779",
     "url": "/portfolio/static/css/main.dcdd70ae.chunk.css"
   },
   {
@@ -24,47 +24,47 @@ self.__precacheManifest = [
     "url": "/portfolio/static/media/Roboto-Regular.3e1af3ef.ttf"
   },
   {
+    "revision": "ee7b96fa85d8fdb8c126409326ac2d2b",
+    "url": "/portfolio/static/media/Roboto-Bold.ee7b96fa.ttf"
+  },
+  {
     "revision": "fc84e998bc29b297ea20321e4c90b6ed",
     "url": "/portfolio/static/media/Roboto-Light.fc84e998.ttf"
   },
   {
-    "revision": "89e2666c24d37055bcb60e9d2d9f7e35",
-    "url": "/portfolio/static/media/Roboto-Thin.89e2666c.ttf"
-  },
-  {
-    "revision": "1b75715b7eb71dc749de",
-    "url": "/portfolio/static/js/main.cf350a87.chunk.js"
-  },
-  {
-    "revision": "42bbe4eefcde1297b11dc4b6491e9746",
-    "url": "/portfolio/static/media/Roboto-Italic.42bbe4ee.ttf"
-  },
-  {
-    "revision": "1eb7a893589ddce89d81cdb22a356660",
-    "url": "/portfolio/static/media/Roboto-BoldItalic.1eb7a893.ttf"
+    "revision": "2d3dd869de5cc9890779",
+    "url": "/portfolio/static/js/main.264e77e1.chunk.js"
   },
   {
     "revision": "d08840599e05db7345652d3d417574a9",
     "url": "/portfolio/static/media/Roboto-Medium.d0884059.ttf"
   },
   {
-    "revision": "d1efcd4d126837fe0dcf9b6cf3a00d64",
-    "url": "/portfolio/static/media/Roboto-LightItalic.d1efcd4d.ttf"
-  },
-  {
     "revision": "0fc25386220a58203994ce45fb4ae570",
     "url": "/portfolio/static/media/Roboto-ThinItalic.0fc25386.ttf"
+  },
+  {
+    "revision": "42bbe4eefcde1297b11dc4b6491e9746",
+    "url": "/portfolio/static/media/Roboto-Italic.42bbe4ee.ttf"
   },
   {
     "revision": "bd19ad60600a1537c00d3b4923a5e5de",
     "url": "/portfolio/static/media/Roboto-MediumItalic.bd19ad60.ttf"
   },
   {
+    "revision": "1eb7a893589ddce89d81cdb22a356660",
+    "url": "/portfolio/static/media/Roboto-BoldItalic.1eb7a893.ttf"
+  },
+  {
+    "revision": "d1efcd4d126837fe0dcf9b6cf3a00d64",
+    "url": "/portfolio/static/media/Roboto-LightItalic.d1efcd4d.ttf"
+  },
+  {
     "revision": "b9df2c36664870e7108a6b59c35cb3b5",
     "url": "/portfolio/static/media/intro.b9df2c36.jpg"
   },
   {
-    "revision": "4194efd611808d8ffac1030947d6f456",
+    "revision": "412b6a0a5aabc08344448e7b080b6fb2",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.1a1fa09bf495d6395be57b4e4f694d07.js"
+  "/portfolio/precache-manifest.95453f0ca768739c1ee975d6c46505e2.js"
 );
 
 workbox.clientsClaim();
