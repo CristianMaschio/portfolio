55f747139e025baee13ef92920af30c55f6e4640
CristianMaschio <blackspartan@hotmail.it>
2020-01-19 19:12:06 +0000

    Updates

---
+++ precache-manifest.070cfbc496a9b1bcfa12009b97439313.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3c19352fc4d0a754078",
+    "url": "/portfolio/static/css/main.eb541791.chunk.css"
+  },
+  {
+    "revision": "d3c19352fc4d0a754078",
+    "url": "/portfolio/static/js/main.4d2aef6b.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "9db34a583a68b6315c49",
+    "url": "/portfolio/static/js/2.02a4d4b1.chunk.js"
+  },
+  {
+    "revision": "37b9aa7e35ddec62992b111b3ead0396",
+    "url": "/portfolio/static/media/imageProfile.37b9aa7e.jpg"
+  },
+  {
+    "revision": "b9df2c36664870e7108a6b59c35cb3b5",
+    "url": "/portfolio/static/media/intro.b9df2c36.jpg"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/portfolio/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "42bbe4eefcde1297b11dc4b6491e9746",
+    "url": "/portfolio/static/media/Roboto-Italic.42bbe4ee.ttf"
+  },
+  {
+    "revision": "ee7b96fa85d8fdb8c126409326ac2d2b",
+    "url": "/portfolio/static/media/Roboto-Bold.ee7b96fa.ttf"
+  },
+  {
+    "revision": "89e2666c24d37055bcb60e9d2d9f7e35",
+    "url": "/portfolio/static/media/Roboto-Thin.89e2666c.ttf"
+  },
+  {
+    "revision": "1eb7a893589ddce89d81cdb22a356660",
+    "url": "/portfolio/static/media/Roboto-BoldItalic.1eb7a893.ttf"
+  },
+  {
+    "revision": "0fc25386220a58203994ce45fb4ae570",
+    "url": "/portfolio/static/media/Roboto-ThinItalic.0fc25386.ttf"
+  },
+  {
+    "revision": "fc84e998bc29b297ea20321e4c90b6ed",
+    "url": "/portfolio/static/media/Roboto-Light.fc84e998.ttf"
+  },
+  {
+    "revision": "d08840599e05db7345652d3d417574a9",
+    "url": "/portfolio/static/media/Roboto-Medium.d0884059.ttf"
+  },
+  {
+    "revision": "d1efcd4d126837fe0dcf9b6cf3a00d64",
+    "url": "/portfolio/static/media/Roboto-LightItalic.d1efcd4d.ttf"
+  },
+  {
+    "revision": "bd19ad60600a1537c00d3b4923a5e5de",
+    "url": "/portfolio/static/media/Roboto-MediumItalic.bd19ad60.ttf"
+  },
+  {
+    "revision": "67cb9f91e03b7fa1297f18e71601d795",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.126e9ef6193a0d62ea46a0fa77fdfb9c.js"
+  "/portfolio/precache-manifest.070cfbc496a9b1bcfa12009b97439313.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
