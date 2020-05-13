8bc1b9afccb18b39d734c58754d9a791030e8bc4
CristianMaschio <blackspartan@hotmail.it>
2020-05-13 11:05:29 +0100

    Updates

---
+++ precache-manifest.b586e4e8d64b8e0289c2cc4e2ef0651d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2281aef590a36ba74efd63dd78e370b9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6a154c056289224c7525",
+    "url": "/portfolio/static/css/main.5ddf6f1b.chunk.css"
+  },
+  {
+    "revision": "e837c39972fc3f039cd5",
+    "url": "/portfolio/static/js/2.13617694.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.13617694.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a154c056289224c7525",
+    "url": "/portfolio/static/js/main.1bad67cd.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "ee7b96fa85d8fdb8c126409326ac2d2b",
+    "url": "/portfolio/static/media/Roboto-Bold.ee7b96fa.ttf"
+  },
+  {
+    "revision": "1eb7a893589ddce89d81cdb22a356660",
+    "url": "/portfolio/static/media/Roboto-BoldItalic.1eb7a893.ttf"
+  },
+  {
+    "revision": "42bbe4eefcde1297b11dc4b6491e9746",
+    "url": "/portfolio/static/media/Roboto-Italic.42bbe4ee.ttf"
+  },
+  {
+    "revision": "fc84e998bc29b297ea20321e4c90b6ed",
+    "url": "/portfolio/static/media/Roboto-Light.fc84e998.ttf"
+  },
+  {
+    "revision": "d1efcd4d126837fe0dcf9b6cf3a00d64",
+    "url": "/portfolio/static/media/Roboto-LightItalic.d1efcd4d.ttf"
+  },
+  {
+    "revision": "d08840599e05db7345652d3d417574a9",
+    "url": "/portfolio/static/media/Roboto-Medium.d0884059.ttf"
+  },
+  {
+    "revision": "bd19ad60600a1537c00d3b4923a5e5de",
+    "url": "/portfolio/static/media/Roboto-MediumItalic.bd19ad60.ttf"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/portfolio/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "89e2666c24d37055bcb60e9d2d9f7e35",
+    "url": "/portfolio/static/media/Roboto-Thin.89e2666c.ttf"
+  },
+  {
+    "revision": "0fc25386220a58203994ce45fb4ae570",
+    "url": "/portfolio/static/media/Roboto-ThinItalic.0fc25386.ttf"
+  },
+  {
+    "revision": "37b9aa7e35ddec62992b111b3ead0396",
+    "url": "/portfolio/static/media/imageProfile.37b9aa7e.jpg"
+  },
+  {
+    "revision": "b9df2c36664870e7108a6b59c35cb3b5",
+    "url": "/portfolio/static/media/intro.b9df2c36.jpg"
+  }
+]);
