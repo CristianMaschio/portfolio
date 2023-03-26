eb65292d3ffb869cf88d63105c5d40fb5a26eca8
Cristian <cristianmaschio@hotmail.it>
2023-03-26 18:15:54 +0100

    Add background and technologies animation

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,32 +2,22 @@ import React, { PureComponent } from "react";
 import Parallax from "react-rellax";
 import Paragraph from "../../components/Paragraph";
 import ImageProfile from "components/ImageProfile";
-import imgBackground from "../../assets/images/intro.jpg";
 import { Fade } from "react-reveal";
+import TagsCanvas from "react-tags-canvas";
+import background from "../../utils/background";
+import json from "../../assets/languages/En-paragraphs.json";
 
 class Home extends PureComponent {
-  state = {
-    paragraphs: undefined,
-  };
-
   componentDidMount() {
-    this.setState({
-      paragraphs: require(`../../assets/languages/En-paragraphs.json`)
-        .paragraphs,
-    });
+    background();
   }
 
   render() {
+    console.log(json);
     return (
       <div className="home">
         <div className="image-background">
-          <img
-            src={imgBackground}
-            alt="desktop setup"
-            height="100%"
-            width="100%"
-          />
-          <div className="image-overlay" />
+          <canvas id="canvas"></canvas>
         </div>
         <Parallax speed={-3}>
           <div className="intro intro-flex-center">
@@ -51,7 +41,37 @@ class Home extends PureComponent {
               <path d="M 0 58 s 188.29 32 508 32 c 290 0 494 -35 494 -35 v 45 h -1002 z"></path>
             </svg>
           </div>
-          {this.state.paragraphs?.map((paragraph, index) => {
+          <div className="paragraph">
+            <h2 className="paragraphTitle">About me</h2>
+            <TagsCanvas
+              style={{ width: "40rem", height: "40rem" }}
+              textColour="rgba(59, 98, 196, 0.9)"
+              maxSpeed={0.06}
+              weightSize={0.5}
+              shape="sphere"
+              noSelect
+              tags={[
+                { value: "Javascript", weight: 30 },
+                { value: "React", weight: 40 },
+                { value: "HTML5", weight: 20 },
+                { value: "CSS3", weight: 20 },
+                { value: "Typescript", weight: 30 },
+                { value: "Git", weight: 20 },
+                { value: "Redux", weight: 20 },
+                { value: "Redux-Saga", weight: 20 },
+                { value: "NodeJS", weight: 20 },
+                { value: "NextJS", weight: 20 },
+                { value: "Docker", weight: 15 },
+                { value: "React-Native", weight: 20 },
+                { value: "Firebase", weight: 20 },
+                { value: "AWS", weight: 20 },
+                { value: "Gatsby", weight: 20 },
+                { value: "GraphQL", weight: 20 },
+              ]}
+            />
+          </div>
+
+          {json.paragraphs?.map((paragraph, index) => {
             return (
               <Paragraph
                 id={paragraph.id}
